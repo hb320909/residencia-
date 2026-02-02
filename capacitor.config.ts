c90e170c797fa1c29918b722a8eeccd8ed2ffda6
hb320909 <harrybrown160609@gmail.com>
2026-02-02 18:49:04 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'residencia',
+  webDir: 'dist'
+};
+
+export default config;
